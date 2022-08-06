53e7f7fedd026685e090c9104e49be0646cb43ac
Daniel89987 <mxsl003@icloud.com>
2022-08-06 14:55:52 -0700

    update

--- aggregate_table_script.R
+++ aggregate_table_script.R
@@ -5,6 +5,5 @@ housing_df <- read.csv("USA_Housing.csv")
 housing_df$Avg..Area.House.Age <- round(housing_df$Avg..Area.House.Age)
 
 df_group_price <- housing_df %>% group_by(Avg..Area.House.Age) %>%
-  summarise(total_avg_income = round(sum(Avg..Area.Income)),
-            total_avg_price = round(sum(Price)),
+  summarise(total_population = round(sum(Area.Population)),
             .groups = "drop")
