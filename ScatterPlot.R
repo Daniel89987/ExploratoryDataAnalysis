80022a29baf0cf64f27e119ebd9a861879847ee2
Daniel89987 <mxsl003@icloud.com>
2022-08-06 14:57:41 -0700

    update

---
+++ ScatterPlot.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(plotly)
+
+data_set <- read.csv("USA_Housing.csv")
+plot_ly(data = data_set, x = ~Area.Population, y = ~Price, 
+        text = ~Address, type = "scatter", mode = "markers")
