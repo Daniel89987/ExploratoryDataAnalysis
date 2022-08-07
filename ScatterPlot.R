d05870135f2f8d4f5683e135917c61f0d9fe77b8
Daniel89987 <mxsl003@icloud.com>
2022-08-07 16:14:52 -0700

    update

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -3,4 +3,5 @@ library(plotly)
 
 data_set <- read.csv("USA_Housing.csv")
 plot_ly(data = data_set, x = ~Area.Population, y = ~Price, 
-        text = ~Address, type = "scatter", mode = "markers")
+        text = ~Address, type = "scatter", mode = "markers") %>%
+  layout(title = "House Price and the population in the houses' area")
