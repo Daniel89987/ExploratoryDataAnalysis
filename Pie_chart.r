a80746c2d3f4af770f0d21b6835a849e0322a654
ericz13 <zhaxu@uw.edu>
2022-08-07 17:16:55 -0700

    ExploratoryDataAnalysis

--- Pie_chart.r
+++ Pie_chart.r
@@ -6,7 +6,7 @@ house_df$Avg..Area.House.Age <- round(house_df$Avg..Area.House.Age)
 house_df$Price <- round_any(house_df$Price, 100)
 
 fig <- plot_ly(house_df, labels = ~Avg..Area.House.Age, values = ~Price, type = 'pie')
-fig <- fig %>% layout(title = 'Percentage of house age',
+fig <- fig %>% layout(title = 'Percentage of house price sum in different house age',
                       xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
                       yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
 
